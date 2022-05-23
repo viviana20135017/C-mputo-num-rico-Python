b6e05699b91ac0b8142fb5f956cf60dfaf5f3597
viviana20135017 <31148920+viviana20135017@users.noreply.github.com>
2022-05-23 18:30:17 -0500

    Update Brujula.py

--- 7prob/2do/Brujula.py
+++ 7prob/2do/Brujula.py
@@ -5,4 +5,4 @@ brujula = ['norte', 'este', 'sur', 'oeste']
 def selectRandom(brujula):
   return numpy.random.choice(brujula, 4)
 
-print("Direcciones cardinales", selectRandom(brujula))
+print("Direcciones cardinales: ", selectRandom(brujula))
