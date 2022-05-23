e5861788634e8061a96f65289164e03e4d8f8fbb
viviana20135017 <31148920+viviana20135017@users.noreply.github.com>
2022-05-23 18:15:56 -0500

    Create DistribucionRandom.py

---
+++ 7prob/2do/DistribucionRandom.py
@@ -0,0 +1,12 @@
+''' 
+Tarea : random.gauss()
+    Los pesos de unos gatos se distribuyen normalmente, con media 4.23 kg
+    y desviacion tipica 0.76kg
+
+Los pesos de los gatos que estan a menos de una desviacion tipica de la media.
+'''
+
+import random
+peso=0
+peso = random.gauss(4.23, 0.76)
+print('Peso de los gatos que estan a menos de una desviacion tipica de la media:', peso)
