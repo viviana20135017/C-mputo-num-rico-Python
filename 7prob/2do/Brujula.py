f064d9a1e50df583d2b56ace74e0e29ffee828cc
viviana20135017 <31148920+viviana20135017@users.noreply.github.com>
2022-05-22 20:26:22 -0500

    Create Brujula.py

---
+++ 7prob/2do/Brujula.py
@@ -0,0 +1,8 @@
+import numpy 
+
+brujula = ['norte', 'este', 'sur', 'oeste']
+
+def selectRandom(brujula):
+  return numpy.random.choice(brujula, 4)
+
+print("Direcciones cardinales", selectRandom(brujula))
