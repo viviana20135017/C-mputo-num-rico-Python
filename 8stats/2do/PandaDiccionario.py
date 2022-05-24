3af5055801fcbb2069b7bf565c3b9c4429b17f06
viviana20135017 <31148920+viviana20135017@users.noreply.github.com>
2022-05-23 21:24:31 -0500

    Create PandaDiccionario.py

---
+++ 8stats/2do/PandaDiccionario.py
@@ -0,0 +1,3 @@
+import pandas as pd
+diplomado = pd.Series({"Módulo I.- Internet de las cosas": 10,  "Módulo II.- Programación móvil y web": 10, "Módulo III.- Cómputo numérico con Python": "?"})
+print(diplomado)
